28c6f2fb8d5c241762822bc1a812f2548638b015
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-03-05 09:32:23 +1100

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-python -m pip install -U scikit-image matplotlib
+python -m pip install -U scikit-image matplotlib ftfy regex tqdm
+$ pip install git+https://github.com/openai/CLIP.git@40f5484c1c74edd83cb9cf687c6ab92b28d8b656
 mkdir datasets
 cd datasets
 wget "https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz"
